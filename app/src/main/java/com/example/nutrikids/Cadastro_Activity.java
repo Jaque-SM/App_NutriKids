19d4ddf37a1126be9ac37026a97706deb7ea0656
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-22 23:07:49 -0300

    Uma solução para controle de acesso

--- app/src/main/java/com/example/nutrikids/Cadastro_Activity.java
+++ app/src/main/java/com/example/nutrikids/Cadastro_Activity.java
@@ -1,7 +1,5 @@
 package com.example.nutrikids;
 
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,71 +8,111 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import com.example.nutrikids.modelo.User;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class Cadastro_Activity extends AppCompatActivity {
 
-    static  EditText nome_completo;
-    static  EditText email_barra;
-    static  EditText senha_barra;
-    static  EditText confirme_senha_barra;
+    private EditText nome_completo;
+    private EditText email_barra;
+    private EditText senha_barra;
+    private EditText confirme_senha_barra;
 
-    static String nome;
-    static String email;
-    static String senha;
-    static String confirme_senha;
+    private String nome;
+    private String email;
+    private String senha;
+    private String confirme_senha;
 
-
-    public String testar;
-    Map<String, Object> user;
-    FirebaseFirestore bd;
+    private FirebaseFirestore bd;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-       user=new HashMap<>();
+        bd = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
-        bd=FirebaseFirestore.getInstance();
-
-        email_barra=findViewById(R.id.email_barra);
-        senha_barra=findViewById(R.id.senha_barra);
-        confirme_senha_barra=findViewById(R.id.confirme_barra);
-        nome_completo=findViewById(R.id.nome_completo);
+        email_barra = findViewById(R.id.email_barra);
+        senha_barra = findViewById(R.id.senha_barra);
+        confirme_senha_barra = findViewById(R.id.confirme_barra);
+        nome_completo = findViewById(R.id.nome_completo);
 
     }
+
+    public void AddCadastro(View view) {
+        nome = nome_completo.getText().toString();
+        email = email_barra.getText().toString();
+        senha = senha_barra.getText().toString();
+        confirme_senha = confirme_senha_barra.getText().toString();
+
+        if (Nulidade() != null) {
+
+            User user = new User(null, nome, email, "nutri");
+
+            mAuth.createUserWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(this, task -> salvarNaColecao());
+
+            fazerLogin();
+
+        }
+
+    }
+
+    private void salvarNaColecao() {
+        String uid = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+
+        User user = new User(uid, nome, email, "responsável");
+
+        bd.collection("users").document(uid).set(user).addOnSuccessListener(unused -> {
+            Snackbar.make(nome_completo, "Cadastrado na coleção", Snackbar.LENGTH_SHORT).show();
+        });
+        
+    }
+
+    private void fazerLogin() {
+        mAuth.signInWithEmailAndPassword(email, senha).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent = new Intent(Cadastro_Activity.this, ListaCardapioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
     public String Nulidade() {
 
-        senha=senha_barra.getText().toString();
+        senha = senha_barra.getText().toString();
 
-        Log.d(testar, "Entrou em nulidade");
+        Log.d("testar", "Entrou em nulidade");
 
-        if (TextUtils.isEmpty(nome)){
+        if (TextUtils.isEmpty(nome)) {
             Toast.makeText(this, "O campo nome não pode ser nulo", Toast.LENGTH_SHORT).show();
             return null;
-       }
-        if (TextUtils.isEmpty(email)){
-            Toast.makeText(this,"O campo email não pode ser nulo", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "O campo email não pode ser nulo", Toast.LENGTH_SHORT).show();
             return null;
         }
-        if (TextUtils.isEmpty(senha)){
-            Toast.makeText(this,"O campo senha não pode ser nulo", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(senha)) {
+            Toast.makeText(this, "O campo senha não pode ser nulo", Toast.LENGTH_SHORT).show();
             return null;
         }
-        if (TextUtils.isEmpty(confirme_senha)||!confirme_senha.equals(senha)){
-            Toast.makeText(this,"O campo senha não esta igual a referencia acima",
+        if (TextUtils.isEmpty(confirme_senha) || !confirme_senha.equals(senha)) {
+            Toast.makeText(this, "O campo senha não esta igual a referencia acima",
                     Toast.LENGTH_SHORT).show();
             return null;
 
@@ -82,45 +120,6 @@ public class Cadastro_Activity extends AppCompatActivity {
         return "Nada errado em campos";
 
     }
-
-    public void AddCadastro(View view){
-        nome=nome_completo.getText().toString();
-        email=email_barra.getText().toString();
-        senha=senha_barra.getText().toString();
-        confirme_senha=confirme_senha_barra.getText().toString();
-
-        if (Nulidade()!=null) {
-
-
-            user.put("Nome", nome);
-            user.put("Email", email);
-            user.put("Senha", senha);
-            user.put("Confirme_Senha", confirme_senha);
-
-            Intent intent=new Intent(this, LoginActivity.class);
-
-            bd.collection("/Nutricionista").document("nutricionista")
-                    .set(user)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(testar, "Dado add com sucesso");
-                            Toast.makeText(Cadastro_Activity.this, "Dados cadastrados", Toast.LENGTH_LONG).show();
-                            //startActivity(intent);
-
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(testar, "Deu erro aki");
-
-                        }
-                    });
-        }
-
-        }
 
 
 }
---
+++ app/src/main/java/com/example/nutrikids/modelo/User.java
@@ -0,0 +1,61 @@
+package com.example.nutrikids.modelo;
+
+public class User {
+
+    private String id;
+    private String nome;
+    private String email;
+    private String tipo;
+
+    public User() {
+    }
+
+    public User(String id, String nome, String email, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
+}
